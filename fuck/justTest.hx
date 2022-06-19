847ce7df404f27a971bf636cf60d9bbcc8ca7fdf
nmslq <96362104+nmslq@users.noreply.github.com>
2022-06-19 12:35:31 +0800

    just fix

--- fuck/justTest.hx
+++ fuck/justTest.hx
@@ -1,1 +1,22 @@
+package test;
+
+import test;
+import just;
+import fuxk;
+import fixG;
+
+using StringTools;
+
+class justTest {
+	public static var testList:Array<String> =
+	[
+		'',
+		'fuck',
+		'you!',
+		'your',
+		'mother',
+		'died!'
+	];
+}
+
 
